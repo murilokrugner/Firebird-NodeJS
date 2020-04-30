f4f88cc4d87b25b70ddb8c5493aa289d77d2ce8e
murilokrugner <murilokrugner@hotmail.com>
2020-04-30 14:49:15 -0300

    conectando ao um banco firebird

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,40 @@
 import { Router } from 'express';
+import firebird from 'node-firebird';
 
 import TestController from './app/controllers/TestController';
 
 const routes = new Router();
 
+const fb_host = '127.0.0.1';
+const fb_port = 3070; 
+const fb_database = 'C:/Bancos/NUTRI/GF/gf2.fdb';
+
+var options = {};
+options.host = fb_host;
+options.port = fb_port;
+options.database = fb_database;
+options.user = 'SYSDBA';
+options.password = 'masterkey';
+
 routes.get('/', TestController.index);
+
+routes.get('/firebird', (req, res) => {
+	firebird.attach(options, function(err, db){
+        if(err){
+            console.log(err.message);
+            res.json('{connection: false}');
+        }
+
+        db.query('SELECT EMP_NOME FROM EMPRESA', function(err, result){
+            if(err){
+                console.log(err.message);
+                res.json('{connection: false}');
+            }
+
+            db.detach();
+            res.json(result);
+        });
+    });
+});
 
 export default routes;
